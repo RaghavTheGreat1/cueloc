8625a74bed5f7bfec83943b4dc61502db0633196
Raghav Joshi <developer.raghavjoshi@gmail.com>
2023-01-29 11:21:40 +0530

    move color palette to separate screen

---
+++ lib/screens/color_palette/color_palette_screen.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette extends StatelessWidget {
+  const ColorPalette({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Scaffold(
+      body: Row(
+        children: [
+          Column(
+            children: [
+              const Text("Primary"),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.primary,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.onPrimary,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.primaryContainer,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.onPrimaryContainer,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const Text("Secondary"),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.secondary,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.onSecondary,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.secondaryContainer,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.onSecondaryContainer,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const Text("Tertiary"),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.tertiary,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.onTertiary,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.tertiaryContainer,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.onTertiaryContainer,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const Text("Background\n&\nSurface"),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.background,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.onBackground,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.surface,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.onSurface,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const Text("Surface\n&\nOutline"),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.surfaceVariant,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.onSurfaceVariant,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.outline,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.outlineVariant,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const Text("Errors"),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.error,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.onError,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.errorContainer,
+              ),
+              Container(
+                height: 48,
+                width: 48,
+                color: theme.colorScheme.onErrorContainer,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
